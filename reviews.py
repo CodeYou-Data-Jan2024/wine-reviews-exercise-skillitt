2a609c7e920cb64b052fdba3b578a199291d7b00
skillitt <markdevore@outlook.com>
2024-05-03 18:03:18 -0400

    Just starting - needing more info to complete

--- reviews.py
+++ reviews.py
@@ -1,2 +1,5 @@
 # add your code here.
 
+import pandas as pd
+reviews = pd.read_csv("/Users/markdevore/github-classroom/CodeYou-Data-Jan2024/wine-reviews-exercise-skillitt/data", index_col=0)
+pd.DataFrame(['Country', 'Count', 'Points'])
