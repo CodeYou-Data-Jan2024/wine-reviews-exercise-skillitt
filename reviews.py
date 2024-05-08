4b848a671e8a344ae4a2eb52f107dbc6cafe7911
skillitt <markdevore@outlook.com>
2024-05-08 07:34:26 -0400

    Round up script edit

--- reviews.py
+++ reviews.py
@@ -17,15 +17,15 @@ summary_df.rename(columns={'country': 'number_of_reviews', 'points': 'average_po
 # Reset index 
 summary_df.reset_index(inplace=True)
 
+# Round up the output CSV
+summary_df['number_of_reviews'] = summary_df['number_of_reviews'].apply(lambda x: int(x) if x % 1 == 0 else int(x) + 1)
+summary_df['average_points'] = summary_df['average_points'].apply(lambda x: round(x))
+
 # output CSV file
 output_csv_path = "data/reviews-per-country.csv"
 
 # Write the summary data to a new CSV file
 summary_df.to_csv(output_csv_path, index=False)
-
-# Round up the average points
-
-summary_df['average_points'] = summary_df['average_points'].round(1)
 
 
 print("Summary data saved to reviews-per-country.csv")
