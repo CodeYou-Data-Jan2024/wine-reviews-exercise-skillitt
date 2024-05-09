e95a102f578b1eb4bf3eb024e9d378bc6bb8e511
skillitt <markdevore@outlook.com>
2024-05-09 08:16:00 -0400

    Final code

--- reviews.py
+++ reviews.py
@@ -1,31 +1,14 @@
 # add your code here.
 
+# import pandas 
 import pandas as pd
 
-# Path to CSV file
-input_csv_path = "data/winemag-data-130k-v2.csv"
+# Read wine mag data
+wine_reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip")
 
-# Read the CSV file 
-df = pd.read_csv(input_csv_path)
+# Group country, count and points
+reviews_country = wine_reviews.groupby(['country']).points.agg([len, "mean"]).round(decimals=1)
+reviews_country = reviews_country.rename(columns={"county": 'country', 'len': "count", 'mean': "points"})
 
-# Group the data by country and calculate the points
-summary_df = df.groupby('country').agg({'country': 'count', 'points': 'mean'})
-
-# Rename columns 
-summary_df.rename(columns={'country': 'number_of_reviews' , 'points': 'average_points'}, inplace=True)
-
-# Reset index 
-summary_df.reset_index(inplace=True)
-
-# Round up the output CSV
-summary_df['number_of_reviews'] = summary_df['number_of_reviews'].apply(lambda x: int(x) if x % 1 == 0 else int(x) + 1)
-summary_df['average_points'] = summary_df['average_points'].apply(lambda x: round(x, 1))
-
-# output CSV file
-output_csv_path = "data/reviews-per-country.csv"
-
-# Write the summary data to a new CSV file
-summary_df.to_csv(output_csv_path, index=False)
-
-
-print("Summary data saved to reviews-per-country.csv")
+# create a file to the data folder
+reviews_country.to_csv("data/reviews-per-country.csv")
